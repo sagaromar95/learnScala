d2f0f9624033c71f6611286d28c056bf7f7582db
sagaromar95 <sagar.omar21@gmail.com>
2021-11-02 16:33:21 +0530

    Adding Assignments

--- src/main/scala/Assignment2/Arrays/question3.scala
+++ src/main/scala/Assignment2/Arrays/question3.scala
@@ -1,5 +1,18 @@
 package Assignment2.Arrays
 
 class question3 extends App {
+  def maxDifference(array : Array[Int]) : Int = {
+    var maxValue : Int = Int.MinValue
+    if (array.length == 0) return 0
+    for (i <- 0 to array.length - 1) {
+      for(j <- i+1 to array.length - 1){
+        if(array(j) > array(i) && (array(j) - array(i) > maxValue)){
+          maxValue = array(j) - array(i)
+        }
+      }
+    }
+    maxValue
+  }
 
+  println(maxDifference(Array(0, 3, 1, 7, 9, 5, 11, 3, 5)))
 }
