ce64ee43ca2c04c00f9088a45c956918475bb94c
sagaromar95 <sagar.omar21@gmail.com>
2021-11-03 18:57:10 +0530

    Adding Assignment 3 => question 4 and question 5

--- src/main/scala/Assignment3/question5.scala
+++ src/main/scala/Assignment3/question5.scala
@@ -1,5 +1,11 @@
 package Assignment3
-
+/*
+Write a function called “conditional” that takes a value x and two functions, p and f, and returns a value of the same type as x.
+The p function is a predicate, taking the value x and returning a Boolean b.
+The f function also takes the value x and returns a new value of the same type.
+Your “conditional” function should only invoke the function f(x) if p(x) is true,
+and otherwise return x. How many type parameters will the “conditional” function require?
+ */
 object question5 extends App {
     def conditional[A] (x: A,p : (A => Boolean),f : (A => A)) : A = {
         if(p(x)) f(x)
